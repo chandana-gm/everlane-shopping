5939c31a82bfaf26655ec4f51d78d979a4acf753
chandana-gm <chandanagm777@gmail.com>
2024-07-31 10:49:23 +0530

    data added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', loadChildren: () => import('./main/main.module').then(m => m.MainModule) }, 
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'shopping', loadChildren: () => import('./shopping/shopping.module').then(m => m.ShoppingModule) }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
