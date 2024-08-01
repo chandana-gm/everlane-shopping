33177b8489b55eb85c3720a1af061e992c2de536
unknown <anjali.cgs@gmail.com>
2024-08-01 14:30:59 +0530

    ff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
 
 const routes: Routes = [
+  {path:'header',component:HeaderComponent},
   { path: 'main', loadChildren: () => import('./main/main.module').then(m => m.MainModule) }, 
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
   { path: 'shopping', loadChildren: () => import('./shopping/shopping.module').then(m => m.ShoppingModule) }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import {  FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent  } from './layout/header/header.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent 
+  
+
+    
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+   
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
