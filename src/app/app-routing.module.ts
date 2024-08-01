d1a1dfdbfc8faef9fe6ca6d74165c84e6bb0b5d2
chandana-gm <chandanagm777@gmail.com>
2024-08-01 14:04:33 +0530

    edited home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'main', loadChildren: () => import('./main/main.module').then(m => m.MainModule) }, 
+  { path: '', loadChildren: () => import('./main/main.module').then(m => m.MainModule) }, 
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
   { path: 'shopping', loadChildren: () => import('./shopping/shopping.module').then(m => m.ShoppingModule) }
   ];
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+    baseUrl: 'https://xd7q7vf5-8000.inc1.devtunnels.ms/api/',
+    mensCategoryUrl: 'subcategories/?category_id=1',
+    womensCategoryUrl: 'subcategories/?category_id=2'
+  };
+export const baseUrl = {
+    production: false,
+    baseUrl: 'https://xd7q7vf5-8000.inc1.devtunnels.ms/api/',
+  };
