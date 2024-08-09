ee2293d7f6f8e34e61823cde5051380f78306c80
unknown <anjali.cgs@gmail.com>
2024-08-09 16:52:52 +0530

    design

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { PostServiceService } from 'src/app/service/post-service.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +10,7 @@ import { PostServiceService } from 'src/app/service/post-service.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  constructor(private fb:FormBuilder,private service:PostServiceService){}
+  constructor(private fb:FormBuilder,private service:PostServiceService,private router:Router,private toastr:ToastrService){}
   email: string = '';
   password: string = '';
 
@@ -30,12 +32,24 @@ if(this.loginForm.valid)
   const formdata=this.loginForm.value 
   this.service.postLogin(formdata).subscribe((data:any)=>{
   console.log('loginResponse',data);
+  this.toastr.success(data.response, 'Success');
+  this.router.navigate(['/main/home']);
+  },
   
-})
+  (error) => {
+
+    console.error('Registration error:', error);
+    this.toastr.error('Login failed. Please try again.', 'Error');
+  
+  
+}
+
+);
+this.loginForm.reset();
 
 }
+  }
+  }
 
   
 
-}
-}
